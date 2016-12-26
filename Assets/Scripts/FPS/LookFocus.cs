be95a5353e64f02ecd02aa52da8e2b391c6847d9
AnotherCat <artmuk.for.game@gmail.com>
2016-12-26 23:15:42 +0700

    add shadder outlined_shadder

---
+++ Assets/Scripts/FPS/LookFocus.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookFocus : MonoBehaviour {
+
+    public Camera fpsCam;
+    private RaycastHit hitInfo;
+    public float maxDistance = 3;
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.TransformDirection(Vector3.forward), out hitInfo, maxDistance))
+        {
+            if(hitInfo.collider != null && hitInfo.collider.tag.Contains("Outlined_Shadder"))
+            {
+
+            }
+        }
+    }
+}
