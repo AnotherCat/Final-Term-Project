6204daf80c3e8a7b3470bd0c0c823b5bb165ceb6
AnotherCat <artmuk.for.game@gmail.com>
2016-12-27 16:35:30 +0700

    55555+

---
+++ Assets/Scripts/ActiveDoor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveDoor : ActiveableObject
+{
+
+    public bool opened = false;
+    
+    public Animation doorAnimation;
+
+    public override void ActiveObject()
+    {
+        if (opened)
+        {
+            CloseDoor();
+        }else
+        {
+            OpenDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        doorAnimation.Play("open");
+        opened = true;
+    }
+    void CloseDoor()
+    {
+        doorAnimation.Play("close");
+        opened = false;
+    }
+
+    private void Update()
+    {
+
+        if (doorAnimation.IsPlaying("open"))
+        {
+            CanActive = false;
+        }
+
+        if (doorAnimation.IsPlaying("close"))
+        {
+            CanActive = false;
+        }
+    }
+
+}
--- Assets/Scripts/FPS/LookFocus.cs
+++ Assets/Scripts/FPS/LookFocus.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookFocus : MonoBehaviour {
 
+    public Text ActiveObjectText;
     public Camera fpsCam;
     private RaycastHit hitInfo;
     public float maxDistance = 3;
@@ -12,9 +14,19 @@ public class LookFocus : MonoBehaviour {
     {
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.TransformDirection(Vector3.forward), out hitInfo, maxDistance))
         {
-            if(hitInfo.collider != null && hitInfo.collider.tag.Contains("Outlined_Shadder"))
+            ActiveableObject act = hitInfo.collider.GetComponent<ActiveableObject>();
+            if(hitInfo.collider != null && act != null)
             {
-
+                if (act.CanActive)
+                {
+                    ActiveObjectText.enabled = true;
+                }else
+                {
+                    ActiveObjectText.enabled = false;
+                }
+            }else
+            {
+                ActiveObjectText.enabled = false;
             }
         }
     }
