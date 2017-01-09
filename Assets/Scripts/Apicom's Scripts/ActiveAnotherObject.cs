aff170bbfe7771a41e0385defe1a086c18a28e99
AnotherCat <artmuk.for.game@gmail.com>
2017-01-09 08:27:25 +0700

    active another object

---
+++ Assets/Scripts/Apicom's Scripts/ActiveAnotherObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveAnotherObject : ActiveButton
+{
+    public ActiveableObject[] TargetObjects;
+
+    public override void ActiveObject()
+    {
+        if (TargetObjects.Length == 0 || TargetObjects == null) return;
+
+        for(int i = 0;i < TargetObjects.Length; i++)
+        {
+            if(TargetObjects[i].Locked == true)
+            {
+                TargetObjects[i].Locked = false;
+            }
+
+            TargetObjects[i].ActiveObject();
+        }
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
