d6c5e540714728a2cc85a09406b658eb84c832ea
AnotherCat <artmuk.for.game@gmail.com>
2017-01-18 21:14:00 +0700

    minigame 0.0.1

---
+++ Assets/Scripts/Apicom's Scripts/MiniGames/MiniGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGame : MonoBehaviour {
+    
+	void Start () {
+
+	}
+	
+    public void onCorrect()
+    {
+        Debug.Log("Correct!!!");
+    }
+
+    public void onIncorrect()
+    {
+        Debug.Log("Incorrect!!!");
+    }
+
+    void Update () {
+		
+	}
+}
