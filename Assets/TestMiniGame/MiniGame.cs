07b756835203791a68ac59c463688bdf0daa5215
Kawisara Issama-ael <Kawisara Issama-ael>
2017-01-25 19:00:52 +0700

    miniGame

---
+++ Assets/TestMiniGame/MiniGame.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MiniGame : MonoBehaviour {
+
+    public string[] Ans;
+    public Text[] text;
+    public Text w;
+
+    bool correct = true;
+
+
+  //  // Update is called once per frame
+  //  void Update () {
+		//if(Ans[0] == text[0].text && Ans[1] == text[1].text&& Ans[2] == text[2].text)
+  //      {
+  //          Debug.Log("Win");
+  //      }
+  //      else
+  //      {
+  //          Debug.Log("Try Agian");
+  //      }
+
+  //  }
+
+    public void checkCorrect()
+    {
+        for (int i = 0; i < Ans.Length; i++)
+        {
+            if (Ans[i] != text[i].text)
+            {
+                
+                correct = false;
+                break;
+            }
+            else
+            {
+                correct = true;
+            }
+
+        }
+
+        Debug.Log(correct);
+        if (correct)
+        {
+            Debug.Log("Winner");
+            w.text = "Winner";
+        }
+        
+    }
+}
---
+++ Assets/TestMiniGame/test.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class test : MonoBehaviour {
+    public Text text;
+
+    int next = 1;
+
+    public void OnNext()
+    {
+        next++;
+        if (next == 1)
+        {
+            text.text = "Up";
+        }
+        else if (next == 2)
+        {
+            text.text = "Down";
+        }
+        else if (next == 3)
+        {
+            text.text = "Right";
+        }
+        else if (next == 4)
+        {
+            text.text = "Left";
+            next = 0;
+        }
+    }
+}
