9bb19bc9176b280837621026d164e6ee5ab1a376
Kawisara Issama-ael <Kawisara Issama-ael>
2017-02-28 22:50:37 +0700

    card game

---
+++ Assets/Minigame/cardGame/Draggable.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Transform parentToreturnTo = null;
+
+    public enum slot {INVENTORY, SKY, SKI, SPIRIT}
+    public slot TypeOfItem = slot.SKY;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        //Debug.Log("On");
+
+        parentToreturnTo = this.transform.parent;
+        this.transform.SetParent(this.transform.parent.parent);
+
+        GetComponent<CanvasGroup>().blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        //Debug.Log("Drag");
+        this.transform.position = eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        //Debug.Log("End");
+        this.transform.SetParent(parentToreturnTo);
+        GetComponent<CanvasGroup>().blocksRaycasts = true;
+    }
+}
---
+++ Assets/Minigame/cardGame/DropZone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public Draggable.slot TypeOfItem = Draggable.slot.SKY;
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "On drop to"+ gameObject.name);
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if(d != null)
+        {
+            if(TypeOfItem == d.TypeOfItem || TypeOfItem == Draggable.slot.INVENTORY)
+            {
+                d.parentToreturnTo = this.transform;
+            }
+            
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //Debug.Log("On pointer Enter");
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //Debug.Log("On pointer Exit");
+    }
+}
