fd868ddbcb837305c5bb543db17a5ed3998efa9b
Kawisara Issama-ael <Kawisara Issama-ael>
2017-03-01 20:55:26 +0700

    jugsaw

--- Assets/Minigame/cardGame/Draggable.cs
+++ Assets/Minigame/cardGame/Draggable.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Transform parentToreturnTo = null;
+    public Transform placeholderParent = null;
+
+    GameObject placeholder = null;
 
     public enum slot {INVENTORY, SKY, SKI, SPIRIT}
     public slot TypeOfItem = slot.SKY;
@@ -14,8 +18,18 @@ public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDrag
     public void OnBeginDrag(PointerEventData eventData)
     {
         //Debug.Log("On");
+        placeholder = new GameObject();
+        placeholder.transform.SetParent(this.transform.parent);
+        LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        le.preferredWidth = this.GetComponent<LayoutElement>().preferredHeight;
+        le.flexibleHeight = 0;
+        le.flexibleWidth = 0;
+
+        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
         parentToreturnTo = this.transform.parent;
+        placeholderParent = parentToreturnTo;
         this.transform.SetParent(this.transform.parent.parent);
 
         GetComponent<CanvasGroup>().blocksRaycasts = false;
@@ -25,12 +39,37 @@ public class Draggable : MonoBehaviour,IBeginDragHandler, IDragHandler, IEndDrag
     {
         //Debug.Log("Drag");
         this.transform.position = eventData.position;
+
+        if (placeholder.transform.parent != placeholderParent)
+            placeholder.transform.SetParent(placeholderParent);
+
+        int newSiblingIndex = placeholderParent.childCount;
+
+        for(int i=0; i < placeholderParent.childCount; i++)
+        {
+            if(this.transform.position.x < placeholderParent.GetChild(i).position.x)
+            {
+                newSiblingIndex = i;
+
+                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+                {
+                    newSiblingIndex--;
+                }
+                  
+                break;
+            }
+        }
+
+        placeholder.transform.SetSiblingIndex(newSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         //Debug.Log("End");
         this.transform.SetParent(parentToreturnTo);
+        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+        Destroy(placeholder);
     }
 }
--- Assets/Minigame/cardGame/DropZone.cs
+++ Assets/Minigame/cardGame/DropZone.cs
@@ -8,27 +8,48 @@ public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPoin
 {
     public Draggable.slot TypeOfItem = Draggable.slot.SKY;
 
-    public void OnDrop(PointerEventData eventData)
-    {
-        Debug.Log(eventData.pointerDrag.name + "On drop to"+ gameObject.name);
-        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
-        if(d != null)
-        {
-            if(TypeOfItem == d.TypeOfItem || TypeOfItem == Draggable.slot.INVENTORY)
-            {
-                d.parentToreturnTo = this.transform;
-            }
-            
-        }
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("On pointer Enter");
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            if (TypeOfItem == d.TypeOfItem || TypeOfItem == Draggable.slot.INVENTORY)
+            {
+                d.placeholderParent = this.transform;
+            }
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("On pointer Exit");
+        if (eventData.pointerDrag == null)
+            return;
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null && d.parentToreturnTo==this.transform)
+        {
+            if (TypeOfItem == d.TypeOfItem || TypeOfItem == Draggable.slot.INVENTORY)
+            {
+                d.placeholderParent = d.parentToreturnTo;
+            }
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + "On drop to" + gameObject.name);
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            if (TypeOfItem == d.TypeOfItem || TypeOfItem == Draggable.slot.INVENTORY)
+            {
+                d.parentToreturnTo = this.transform;
+            }
+        }
     }
 }
