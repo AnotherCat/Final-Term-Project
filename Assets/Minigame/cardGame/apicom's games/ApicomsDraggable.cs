3facdc89179f5da03bf2b84a8552d374b2c4ed12
Kawisara Issama-ael <Kawisara Issama-ael>
2017-03-02 19:43:40 +0700

    edit

--- Assets/Minigame/cardGame/apicom's games/ApicomsDraggable.cs
+++ Assets/Minigame/cardGame/apicom's games/ApicomsDraggable.cs
@@ -18,15 +18,15 @@ public class ApicomsDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, 
     public void OnBeginDrag(PointerEventData eventData)
     {
         //Debug.Log("On");
-        placeholder = new GameObject();
-        placeholder.transform.SetParent(this.transform.parent);
-        LayoutElement le = placeholder.AddComponent<LayoutElement>();
-        le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
-        le.preferredWidth = this.GetComponent<LayoutElement>().preferredHeight;
-        le.flexibleHeight = 0;
-        le.flexibleWidth = 0;
+        //placeholder = new GameObject();
+        //placeholder.transform.SetParent(this.transform.parent);
+        //LayoutElement le = placeholder.AddComponent<LayoutElement>();
+        //le.preferredWidth = this.GetComponent<LayoutElement>().preferredWidth;
+        //le.preferredWidth = this.GetComponent<LayoutElement>().preferredHeight;
+        //le.flexibleHeight = 0;
+        //le.flexibleWidth = 0;
 
-        placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
+        //placeholder.transform.SetSiblingIndex(this.transform.GetSiblingIndex());
 
         parentToreturnTo = this.transform.parent;
         placeholderParent = parentToreturnTo;
@@ -37,39 +37,40 @@ public class ApicomsDraggable : MonoBehaviour, IBeginDragHandler, IDragHandler, 
 
     public void OnDrag(PointerEventData eventData)
     {
-        //Debug.Log("Drag");
+        Debug.Log("OnDrag");
         this.transform.position = eventData.position;
 
-        if (placeholder.transform.parent != placeholderParent)
-            placeholder.transform.SetParent(placeholderParent);
+        //if (placeholder.transform.parent != placeholderParent)
+        //    placeholder.transform.SetParent(placeholderParent);
 
-        int newSiblingIndex = placeholderParent.childCount;
+        //int newSiblingIndex = placeholderParent.childCount;
 
-        for (int i = 0; i < placeholderParent.childCount; i++)
-        {
-            if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
-            {
-                newSiblingIndex = i;
+        //for (int i = 0; i < placeholderParent.childCount; i++)
+        //{
+        //    if (this.transform.position.x < placeholderParent.GetChild(i).position.x)
+        //    {
+        //        newSiblingIndex = i;
 
-                if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
-                {
-                    newSiblingIndex--;
-                }
+        //        if (placeholder.transform.GetSiblingIndex() < newSiblingIndex)
+        //        {
+        //            newSiblingIndex--;
+        //        }
 
-                break;
-            }
-        }
+        //        break;
+        //    }
+        //}
 
-        placeholder.transform.SetSiblingIndex(newSiblingIndex);
+        //placeholder.transform.SetSiblingIndex(newSiblingIndex);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        //Debug.Log("End");
+        Debug.Log("End");
         this.transform.SetParent(parentToreturnTo);
-        this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
+        
+        //this.transform.SetSiblingIndex(placeholder.transform.GetSiblingIndex());
         GetComponent<CanvasGroup>().blocksRaycasts = true;
 
-        Destroy(placeholder);
+        //Destroy(placeholder);
     }
 }
--- Assets/Minigame/cardGame/apicom's games/ApicomsDropZone.cs
+++ Assets/Minigame/cardGame/apicom's games/ApicomsDropZone.cs
@@ -11,45 +11,46 @@ public class ApicomsDropZone : MonoBehaviour ,IDropHandler,IPointerEnterHandler,
     public void OnPointerEnter(PointerEventData eventData)
     {
         //Debug.Log("On pointer Enter");
-        if (eventData.pointerDrag == null)
-            return;
+        //if (eventData.pointerDrag == null)
+        //    return;
 
-        ApicomsDraggable d = eventData.pointerDrag.GetComponent<ApicomsDraggable>();
-        if (d != null)
-        {
-            if (TypeOfItem == d.TypeOfItem || TypeOfItem == ApicomsDraggable.slot.table)
-            {
-                d.placeholderParent = this.transform;
-            }
-        }
+        //ApicomsDraggable d = eventData.pointerDrag.GetComponent<ApicomsDraggable>();
+        //if (d != null)
+        //{
+        //    if (TypeOfItem == d.TypeOfItem || TypeOfItem == ApicomsDraggable.slot.table)
+        //    {
+        //        d.placeholderParent = this.transform;
+        //    }
+        //}
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         //Debug.Log("On pointer Exit");
-        if (eventData.pointerDrag == null)
-            return;
+        //if (eventData.pointerDrag == null)
+        //    return;
 
-        ApicomsDraggable d = eventData.pointerDrag.GetComponent<ApicomsDraggable>();
-        if (d != null && d.parentToreturnTo == this.transform)
-        {
-            if (TypeOfItem == d.TypeOfItem || TypeOfItem == ApicomsDraggable.slot.table)
-            {
-                d.placeholderParent = d.parentToreturnTo;
-            }
-        }
+        //ApicomsDraggable d = eventData.pointerDrag.GetComponent<ApicomsDraggable>();
+        //if (d != null && d.parentToreturnTo == this.transform)
+        //{
+        //    if (TypeOfItem == d.TypeOfItem || TypeOfItem == ApicomsDraggable.slot.table)
+        //    {
+        //        d.placeholderParent = d.parentToreturnTo;
+        //    }
+        //}
     }
 
     public void OnDrop(PointerEventData eventData)
     {
-        Debug.Log(eventData.pointerDrag.name + "On drop to" + gameObject.name);
+        //Debug.Log(eventData.pointerDrag.name + "On drop to" + gameObject.name);
         ApicomsDraggable d = eventData.pointerDrag.GetComponent<ApicomsDraggable>();
-        if (d != null)
+        if (d != null && gameObject.transform.childCount > 0 )
         {
+            gameObject.transform.GetChild(0).SetParent(d.parentToreturnTo);
             if (TypeOfItem == d.TypeOfItem || TypeOfItem == ApicomsDraggable.slot.table)
             {
                 d.parentToreturnTo = this.transform;
-            }
+            }          
         }
     }
 }
